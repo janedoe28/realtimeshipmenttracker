6997fbfcc5c6384efa78064ac18832970b41497c
JaneDoe28 <kaitlynleedesigns@gmail.com>
2023-11-12 20:39:11 -0600

    updates for shipmenttracker.3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+import React from 'react';
+import ShipmentTracker from './ShipmentTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your Shipment Tracking App!</h1>
+        <ShipmentTracker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
