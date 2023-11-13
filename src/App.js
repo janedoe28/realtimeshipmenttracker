eb7f03ae17f55c63e5154d90c043ca80c878583d
JaneDoe28 <kaitlynleedesigns@gmail.com>
2023-11-12 19:49:27 -0600

    changes for v3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello</h1>
+        <h1>test</h1>
       </header>
     </div>
   );
